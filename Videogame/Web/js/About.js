24e8445c6cf3e3c4fae7c2f2313b20e835975dcb
pousinnn <pandic.cortez@gmail.com>
2026-06-09 10:33:19 -0600

    minor web changes

--- Videogame/Web/js/About.js
+++ Videogame/Web/js/About.js
@@ -2,6 +2,28 @@
 
 const backBtn = document.getElementById("backBtn");
 
-backBtn.addEventListener("click", () => {
-    window.location.href = "Run_Menu.html";
-});
+if(backBtn){
+    backBtn.addEventListener("click", () => {
+        window.location.href = "Run_Menu.html";
+    });
+}
+
+const userDisplay = document.getElementById("userDisplay");
+const logoutBtn = document.getElementById("logoutBtn");
+
+const username = localStorage.getItem("loggedInUser");
+
+if(username){
+    userDisplay.textContent = "Welcome " + username;
+}
+
+if(logoutBtn){
+    logoutBtn.addEventListener("click", (event) => {
+
+        event.preventDefault();
+
+        localStorage.removeItem("loggedInUser");
+
+        window.location.href = "Run_Menu.html";
+    });
+}
